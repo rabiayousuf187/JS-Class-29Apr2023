818b77a9958f59dea8c0826cd04c5ed991a72a1f
Rabia Yousuf <53699291+rabiayousuf187@users.noreply.github.com>
2023-04-30 10:39:36 +0500

    inital commit
    Chapter 33

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+console.log("JS-SMIT-Class-Chap-32");
+console.log(". a date and time");
+var datenow  = new Date();
+console.log("datenow ====== ", datenow);
+
+datenow  = new Date("June 30, 2035");
+console.log("datenow ====== ", datenow);
+
